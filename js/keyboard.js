46407b7874bd7cacf76d1cf54d2fce705d9f1407
Raj Nakarja <raj@siliconwitchery.com>
2023-02-28 22:41:02 +0100

    Added control keys and tab

--- js/keyboard.js
+++ js/keyboard.js
@@ -10,24 +10,30 @@ let keyboard = new Keyboard({
             "q w e r t y u i o p",
             "a s d f g h j k l",
             "{shift} z x c v b n m {bksp}",
-            "{num} {space} {return}"
+            "{num} {tab} {space} {return}"
         ],
         shift: [
             "Q W E R T Y U I O P",
             "A S D F G H J K L",
             "{shift} Z X C V B N M {bksp}",
-            "{num} {space} {return}"
+            "{num} {tab} {space} {return}"
         ],
         num: [
             "1 2 3 4 5 6 7 8 9 0",
             `- / : ; ( ) $ & $ "`,
             "{alt} . , ? ! ' {bksp}",
-            "{default} {space} {return}"
+            "{default} {ctrl} {space} {return}"
         ],
         alt: [
             "[ ] { } # % ^ * + =",
             `_ \\ | ~ < > "`,
             "{num} . , ? ! ' {bksp}",
+            "{default} {ctrl} {space} {return}"
+        ],
+        ctrl: [
+            "{ctrl-a} {ctrl-b} {ctrl-c} {ctrl-d} {ctrl-e}",
+            `{upload} {clear}`,
+            "{num} {up} {down} {left} {right} {bksp}",
             "{default} {space} {return}"
         ]
     },
@@ -36,9 +42,26 @@ let keyboard = new Keyboard({
         "{shift}": "⇧",
         "{num}": "123",
         "{alt}": "#+=",
-        "{bksp}": "⇦",
+        "{ctrl}": "ctrl",
+
+        "{tab}": "⇥",
+        "{bksp}": "⌫",
         "{space}": " ",
         "{return}": "return",
+
+        "{ctrl-a}": "ctrl-a",
+        "{ctrl-b}": "ctrl-b",
+        "{ctrl-c}": "ctrl-c",
+        "{ctrl-d}": "ctrl-d",
+        "{ctrl-e}": "ctrl-e",
+
+        "{upload}": "upload fpga file",
+        "{clear}": "clear repl",
+
+        "{up}": "⇧",
+        "{down}": "⇩",
+        "{left}": "⇦",
+        "{right}": "⇨",
     }
 });
 
@@ -69,8 +92,16 @@ function onKeyPress(button) {
             keyboard.setOptions({ layoutName: "alt" });
             break;
 
+        case "{ctrl}":
+            keyboard.setOptions({ layoutName: "ctrl" });
+            break;
+
         case "{bksp}":
             replHandleKeyPress("Backspace", false, false);
+            break;
+
+        case "{tab}":
+            replHandleKeyPress("Tab", false, false);
             break;
 
         case "{space}":
