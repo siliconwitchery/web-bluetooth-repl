bfb2691e1702e2176a85ea6e597f09e7703f2eda
te-uma <umasankar@technoexponent.com>
2023-04-11 08:51:00 +0530

    Raw data to image

--- js/main.js
+++ js/main.js
@@ -149,7 +149,85 @@ export function reportUpdatePercentage(percentage) {
     infoText.innerHTML = "Updating " + percentage.toFixed(2) + "%";
 }
 
-// TODO
+// NEED Testing and validation by Raj /Joshua
+//  -by uma
+
+/** converting raw data to image */
+// Image Variables
+let image_final_buffer = null;
+let image_file_name = null;
+let image_file_size = null;
+
+// Whenever raw data arrives over bluetooth
 export function receiveRawData(event) {
     console.log(event.target.value);
+
+    try{
+        if(appendBuffer(event.target.value.buffer)){
+            showImage()
+            console.log(`finished transfer: ${image_file_name} -  ${image_final_buffer.byteLength} of ${image_file_size}`)
+            
+        }else{
+            console.log(`progress: ${image_file_name} -  ${image_final_buffer.byteLength} of ${image_file_size}`)
+        }
+        image_final_buffer = null;
+        image_file_size = null;
+        image_file_name = null;
+    }catch(error){
+        console.log(error)
+    }
+    
 }
+
+//for displaying recieved imaged
+ function showImage(){
+    let file_ext = image_file_name.split('.').pop()
+    let img_blob = new Blob([image_final_buffer], { type: "image/"+file_ext });
+    let urlCreator = window.URL || window.webkitURL;
+    let imageUrl = urlCreator.createObjectURL(img_blob);
+    // let img = document.querySelector("#image");
+    // img.src = imageUrl;
+    let a = document.createElement('a');
+    a.href = imageUrl;
+    a.download = image_file_name;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    
+   
+ }
+
+// concatinating data function
+const appendBuffer = function (buffer) {
+    let w_temp = new Uint8Array(buffer)
+
+//FLAGS for data read
+    const FILE_INDEX = w_temp[0]
+    const FILE_SIZE_START = 1
+    const FILE_SIZE_BITS = 4
+    const FILE_NAME_LENGTH_SIZE = 5
+    const FILE_NAME_LENGTH_BIT= 1
+
+    const FILE_NAME_START = FILE_NAME_LENGTH_SIZE + FILE_NAME_LENGTH_BIT
+
+    if (FILE_INDEX === 1 || FILE_INDEX === 0) {
+
+        image_file_size = new Int32Array(w_temp.slice(FILE_SIZE_START, FILE_SIZE_START+FILE_SIZE_BITS).buffer).toString(10)
+        image_file_name = new TextDecoder("utf-8").decode(w_temp.slice(FILE_NAME_START, w_temp[FILE_NAME_LENGTH_SIZE] + FILE_NAME_START))
+        image_final_buffer = w_temp.slice(w_temp[FILE_NAME_LENGTH_SIZE] + FILE_NAME_START, w_temp.byteLength).buffer
+    }
+    if(FILE_INDEX === 2 || FILE_INDEX === 3){
+        let tmp = new Uint8Array(image_final_buffer.byteLength + w_temp.byteLength-1);
+        tmp.set(new Uint8Array(image_final_buffer), 0);
+        tmp.set(new Uint8Array(w_temp.slice(1, w_temp.byteLength)), image_final_buffer.byteLength);
+        image_final_buffer = tmp.buffer
+    }
+
+    if (FILE_INDEX === 0 || FILE_INDEX === 3) {
+        return true;
+        // payload finished
+    }else{
+        return false;
+    }
+
+};
